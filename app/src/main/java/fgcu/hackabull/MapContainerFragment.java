6f3aac89daabb32ac8565d6828aa7915afd90078
Edgar Meruvia Garron <emeruvia@gmail.com>
2018-03-25 13:38:06 -0400

    Added users in map

--- app/src/main/java/fgcu/hackabull/MapContainerFragment.java
+++ app/src/main/java/fgcu/hackabull/MapContainerFragment.java
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -47,11 +48,23 @@ public class MapContainerFragment extends Fragment implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         //TODO UPdate this part with the locations of the different places
-        LatLng usf = new LatLng(28.0587, 82.4139);
-        MarkerOptions options = new MarkerOptions();
-        options.position(usf).title("USF LAT LONG");
-        map.addMarker(options);
-        map.moveCamera(CameraUpdateFactory.newLatLng(usf));
+        LatLng usf = new LatLng(28.0587713, -82.4154902);
+        LatLng temp1 = new LatLng(28.0596917, -82.4159253);
+        LatLng temp2 = new LatLng(28.0613838,-82.4201508);
+        LatLng temp3 = new LatLng(28.0546556, -82.413537);
+        LatLng temp4 = new LatLng(28.0654595, -82.4096904);
+        LatLng temp5 = new LatLng(28.0631987, -82.4113806);
+//        MarkerOptions options = new MarkerOptions();
+//        options.position(usf).title("Current Position");
+//        map.addMarker(options);
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(usf, 20 ));
+        map.addMarker(new MarkerOptions().position(usf).title("Current Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        map.addMarker(new MarkerOptions().position(temp1).title("Handyman Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        map.addMarker(new MarkerOptions().position(temp2).title("Handyman Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        map.addMarker(new MarkerOptions().position(temp3).title("Needs Assistance").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        map.addMarker(new MarkerOptions().position(temp4).title("Needs Assitance").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(usf, 15));
     }
 }
