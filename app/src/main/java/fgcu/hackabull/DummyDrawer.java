e8df4c6985b9155d3a0f91954ff8e91747e0922e
Edgar Meruvia Garron <emeruvia@gmail.com>
2018-03-24 22:02:00 -0400

    fixed my mistake, sorry

--- app/src/main/java/fgcu/hackabull/DummyDrawer.java
+++ app/src/main/java/fgcu/hackabull/DummyDrawer.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+//TODO Add Fragment to this placeholder for the map api fragment
 public class DummyDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
