cc9c38f73c3a0e3455e8a4cd653939ba21930af2
jeffrey <jeffreyfleurent@outlook.com>
2018-03-25 08:25:15 -0400

    Added classes for items for recyclerview

--- app/src/main/java/fgcu/hackabull/DataObjects/Payment.java
+++ app/src/main/java/fgcu/hackabull/DataObjects/Payment.java
@@ -5,4 +5,46 @@ package fgcu.hackabull.DataObjects;
  */
 
 public class Payment {
+    private String clientName;
+    private String handymanName;
+    private String date;
+    private String amount;
+    public Payment(String clientName, String handymanName, String date, String amount){
+        this.clientName = clientName;
+        this.handymanName = handymanName;
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHandymanName() {
+        return handymanName;
+    }
+
+    public void setHandymanName(String handymanName) {
+        this.handymanName = handymanName;
+    }
 }
--- app/src/main/java/fgcu/hackabull/DataObjects/Request.java
+++ app/src/main/java/fgcu/hackabull/DataObjects/Request.java
@@ -5,4 +5,47 @@ package fgcu.hackabull.DataObjects;
  */
 
 public class Request {
+    private String typeOfHelp;
+    private String date;
+    private String time;
+    private String location;
+
+    public Request(String typeOfHelp,String date, String time, String location){
+        this.typeOfHelp = typeOfHelp;
+        this.date = date;
+        this.time= time;
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTypeOfHelp() {
+        return typeOfHelp;
+    }
+
+    public void setTypeOfHelp(String typeOfHelp) {
+        this.typeOfHelp = typeOfHelp;
+    }
 }
