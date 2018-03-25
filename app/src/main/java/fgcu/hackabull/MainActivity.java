2a0d3c75051f3003ee4b389ba9df7edff3e06521
jeffrey <jeffreyfleurent@outlook.com>
2018-03-25 13:06:22 -0400

    Add login to mainactivity

--- app/src/main/java/fgcu/hackabull/MainActivity.java
+++ app/src/main/java/fgcu/hackabull/MainActivity.java
@@ -85,13 +85,13 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<C
             }
         });
 
-      /*  Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.register_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
             }
-        }); */
+        });
 
       //  mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
